ce090d9f4da67eff8d9c0fc3daf4ff03cc1ef606
udoka ubah <udokaubah9@gmail.com>
2026-06-02 15:40:03 -0700

    fix: clear absolute vercel cors origins for express and web sockets

--- server.js
+++ server.js
@@ -21,7 +21,11 @@ const hpp = require("hpp");
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:3000", process.env.FRONTEND_URL];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://chattrixfrontend.vercel.app",
+  "https://chattrix-frontend.vercel.app",
+];
 
 const server = http.createServer(app);
 
