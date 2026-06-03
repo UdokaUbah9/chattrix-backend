d73555d43139be0caf888ac18c5154867a0c5230
udoka ubah <udokaubah9@gmail.com>
2026-06-03 09:09:45 -0700

    fix: change service to explicit host to unblock port 587

--- utils/nodeMailer.js
+++ utils/nodeMailer.js
@@ -2,17 +2,21 @@ const nodemailer = require("nodemailer");
 
 module.exports = function () {
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    host: "smtp.gmail.com",
+    secure: false,
     auth: {
       user: process.env.NODE_MAILER_GMAIL_USER,
       pass: process.env.NODE_MAILER_GMAIL_PASS,
     },
+    tls: {
+      rejectUnauthorized: false,
+    },
   });
 
   return async function sendMail(option) {
-    transporter
+    await transporter
       .sendMail({
-        from: `"Chattrix App" ${process.env.NODE_MAILER_GMAIL_USER}`,
+        from: `"Chattrix App" <${process.env.NODE_MAILER_GMAIL_USER}>`,
         to: option.email,
         subject: option.subject,
         text: option.text,
