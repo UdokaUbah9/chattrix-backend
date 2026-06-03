8f7d8f56830f56fca24efbca8aec35489bf8566c
udoka ubah <udokaubah9@gmail.com>
2026-06-03 09:40:35 -0700

    initial clean backend commit

--- utils/nodeMailer.js
+++ utils/nodeMailer.js
@@ -2,21 +2,19 @@ const nodemailer = require("nodemailer");
 
 module.exports = function () {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    service: "Gmail",
+    port: 587,
     secure: false,
     auth: {
       user: process.env.NODE_MAILER_GMAIL_USER,
       pass: process.env.NODE_MAILER_GMAIL_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
     },
   });
 
   return async function sendMail(option) {
     await transporter
       .sendMail({
-        from: `"Chattrix App" <${process.env.NODE_MAILER_GMAIL_USER}>`,
+        from: `"Chattrix App" ${process.env.NODE_MAILER_GMAIL_USER}`,
         to: option.email,
         subject: option.subject,
         text: option.text,
