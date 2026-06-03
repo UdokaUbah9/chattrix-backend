44d4971fac8b4d28b53f1bf772d63dcda595e1f7
udoka ubah <udokaubah9@gmail.com>
2026-06-03 11:08:22 -0700

    feat: drop nodemailer transport for resend api client inside createMailer utility

--- utils/nodeMailer.js
+++ utils/nodeMailer.js
@@ -1,26 +1,45 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+
+// module.exports = function () {
+//   const transporter = nodemailer.createTransport({
+//     service: "Gmail",
+//     port: 587,
+//     secure: false,
+//     auth: {
+//       user: process.env.NODE_MAILER_GMAIL_USER,
+//       pass: process.env.NODE_MAILER_GMAIL_PASS,
+//     },
+//   });
+
+//   return async function sendMail(option) {
+//     await transporter
+//       .sendMail({
+//         from: `"Chattrix App" ${process.env.NODE_MAILER_GMAIL_USER}`,
+//         to: option.email,
+//         subject: option.subject,
+//         text: option.text,
+//       })
+//       .catch((err) => {
+//         throw new Error(`Email failed: ${err.message}`);
+//       });
+//   };
+// };
+
+const { Resend } = require("resend");
 
 module.exports = function () {
-  const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.NODE_MAILER_GMAIL_USER,
-      pass: process.env.NODE_MAILER_GMAIL_PASS,
-    },
-  });
+  const resend = new Resend(process.env.RESEND_API_KEY);
 
   return async function sendMail(option) {
-    await transporter
-      .sendMail({
-        from: `"Chattrix App" ${process.env.NODE_MAILER_GMAIL_USER}`,
+    try {
+      await resend.emails.send({
+        from: "onboarding@resend.dev", // Resend provides this default free testing email
         to: option.email,
         subject: option.subject,
         text: option.text,
-      })
-      .catch((err) => {
-        throw new Error(`Email failed: ${err.message}`);
       });
+    } catch (err) {
+      throw new Error(`Email failed: ${err.message}`);
+    }
   };
 };
